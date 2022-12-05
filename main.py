339332ea6dead5959e9b4ccc60a47ae9192e8ce6
finny <79883694+finnycode@users.noreply.github.com>
2022-12-05 13:18:40 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+import time as t
+import random
+
+#take all of the data from the csv file and put it into a dataframe
+df = pd.read_csv('AAPL_data.csv')
+
+
+
+
+#make all date values of df the datetime
+df['date'] = pd.to_datetime(df['date'])
+
+#create a difference model for the close prices in df
+df['Close_diff'] = df['close'].diff()
+
+
+#make df only have the date and close_diff and close columns
+df = df[['date', 'Close_diff', 'close']]
+
+
+profitl = []
+profit = 0
+#for every price in close_diff, if it is above or below its mean + 2*std, then print it and its date and next df['close'] value + profit and such
+for i in range(len(df) - 2):
+    if df['Close_diff'][i] > df['Close_diff'].mean() + 2*df['Close_diff'].std():
+        print("Above")
+        print(df['date'][i], df['Close_diff'][i])
+        print("First close price: ", df['close'][i])
+        print("Next Close Price: ", df['close'][i+1])
+        print("Profit: " + str(df['close'][i+1] - df['close'][i]))
+        profit += df['close'][i+1] - df['close'][i]
+        profitl.append(df['close'][i] - df['close'][i+1])
+        
+    elif df['Close_diff'][i] < df['Close_diff'].mean() - 2*df['Close_diff'].std():
+        print("Below")
+        print(df['date'][i], df['Close_diff'][i])
+        print("First close price: ", df['close'][i])
+        print("Next Close Price: ", df['close'][i+1])
+        print("Profit: " + str(df['close'][i+1] - df['close'][i]))
+        profit += df['close'][i+1] - df['close'][i]
+        profitl.append(df['close'][i+1] - df['close'][i])
+    else:
+        print("Nothing")
+        
+print("/n Total profit: " + str(sum(profitl)))
+print("Max profit: " + str(max(profitl)))
+print("Min profit: " + str(min(profitl)))
+#plot close_diff and the df[date] values
+plt.plot(df['date'], df['Close_diff'])
+
+#plot the close_diff mean
+plt.axhline(df['Close_diff'].mean(), color='red')
+#plot the close_diff 2 population standard deviation + and - mean
+plt.axhline(df['Close_diff'].mean() + 2*df['Close_diff'].std(), color='green')
+plt.axhline(df['Close_diff'].mean() - 2*df['Close_diff'].std(), color='green')
+
+plt.show()
+
